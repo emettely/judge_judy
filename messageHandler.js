461ac62dfdc2732a24be1af1f72808a789507d4f
James Dooley <james.dooley@bbc.co.uk>
2018-11-15 13:50:08 +0530

    Specific info regarding article stories, fixing indentation

--- messageHandler.js
+++ messageHandler.js
@@ -23,46 +23,41 @@ const URL_SOURCE_MESSAGE = {
   falseArticle: 'It seems you have fallen for a fake story 😕 \n\n'
 };
 
-const ocr = (img) => {
-    return {
-      text: "Bengali is the sweetest language said UNESCO"
-    };
-}
-
 const verifyUrl = (message) => {
   let unreliable = {
     source: null,
+    source_info: null,
     article: null,
     article_proof: null,
-    info: null,
+    article_info: null,
     verified: false
   }
 
   if (/http|https|www|.com|.co.uk|.in|.news|.info/.test(message)) {
-      console.log('URL detected in message');
+    console.log('URL detected in message');
 
-      database.blacklist.articles.forEach((article) => {
-          if (message.indexOf(article.url) > -1) {
-              unreliable.article = true;
-              unreliable.article_proof = article.proof;
-              unreliable.info = article.info;
-          }
-      });
+    database.blacklist.articles.forEach((article) => {
+      if (message.indexOf(article.url) > -1) {
+        unreliable.article = true;
+        unreliable.article_proof = article.proof;
+        unreliable.article_info = article.info;
+      }
+    });
 
-      database.blacklist.sources.forEach((source) => {
-          if (message.indexOf(source.url) > -1) {
-              unreliable.source = true;
-              unreliable.info = source.info;
-          }
-      });
+    database.blacklist.sources.forEach((source) => {
+      if (message.indexOf(source.url) > -1) {
+        unreliable.source = true;
+        unreliable.source_info = source.info;
+      }
+    });
 
-      database.whitelist.sources.forEach((source) => {
-          if (message.indexOf(source.url) > -1) {
-              unreliable.verified = true;
-          }
-      });
+    database.whitelist.sources.forEach((source) => {
+      if (message.indexOf(source.url) > -1) {
+        unreliable.verified = true;
+      }
+    });
   }
-    return unreliable;
+  return unreliable;
 }
 
 const checkForRedFlags = (message) => {
@@ -76,10 +71,10 @@ const checkForRedFlags = (message) => {
 };
 
 const replyMessage = (messageId, text) => {
-    return {
-        msgId: messageId,
-        msg: text
-    }
+  return {
+    msgId: messageId,
+    msg: text
+  }
 };
 
 const getCautionaryMessages = (messageId, redFlagged, unreliable) => {
@@ -89,13 +84,14 @@ const getCautionaryMessages = (messageId, redFlagged, unreliable) => {
     messageQueue.push(replyMessage(messageId, RED_FLAG_MESSAGE.false))
   }
 
+  console.log(unreliable);
+
   if (unreliable.verified) {
     messageQueue.push(replyMessage(messageId, URL_SOURCE_MESSAGE.verified));
   } else if (unreliable.article) {
-    const msg = `${URL_SOURCE_MESSAGE.falseArticle}${unreliable.info}${unreliable.article_proof}`
+    const msg = `${URL_SOURCE_MESSAGE.falseArticle}${unreliable.article_info}${unreliable.article_proof}`
     messageQueue.push(replyMessage(messageId, msg));
-  } else if (unreliable.source) {
-    console.log(unreliable)
+  } else if (unreliable.source_info) {
     const reason = unreliable.info || URL_SOURCE_MESSAGE.false;
     messageQueue.push(replyMessage(messageId, `${reason}🚨`));
   } else {
@@ -117,22 +113,21 @@ const verify = (message) => {
   const msgId = message.message_id;
 
   const messageQueue = getCautionaryMessages(
-      msgId,
-      checkForRedFlags(text),
-      verifyUrl(text)
+    msgId,
+    checkForRedFlags(text),
+    verifyUrl(text)
   );
 
   if (messageQueue.length > 0) {
-      messageQueue.push(replyMessage(msgId,
-          `Hey ${username}, you better check what you're sharing first!`));
+    messageQueue.push(replyMessage(msgId,
+      `Hey ${username}, you better check what you're sharing first!`));
 
-  }
-  console.log(messageQueue);
+    }
+    console.log(messageQueue);
 
-  return messageQueue;
-};
+    return messageQueue;
+  };
 
-module.exports = {
-  ocr,
-  verify
-};
+  module.exports = {
+    verify
+  };
